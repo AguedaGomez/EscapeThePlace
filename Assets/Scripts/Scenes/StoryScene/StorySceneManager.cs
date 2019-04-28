0e0bfecf03100ad8d443021e5ada938c2085aaaa
Agueda Gomez Cambronero <al285895@uji.es>
2019-04-28 21:54:56 +0200

    Create StoryScene. Add kitchen, desktop and basement sprites and create its prefabs

---
+++ Assets/Scripts/Scenes/StoryScene/StorySceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StorySceneManager : MonoBehaviour
+{
+    public Canvas canvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadScreen();
+    }
+
+    private void LoadScreen()
+    {
+        //Depends on beacon lecture
+    }
+
+    private void CreateScreen(GameObject prefab)
+    {
+        Instantiate(prefab, canvas.transform);
+    }
+
+}

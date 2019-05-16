17b855fad9789b3e1069bba7a069e37feec62dab
Agueda Gomez <gmz.cambronero@gmail.com>
2019-05-16 16:29:24 +0200

    Modify text and clues. Add new button in menuScene

---
+++ Assets/Scripts/Scenes/MenuScene/LoadNewScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewScene : MonoBehaviour
+{
+    public void ChangeFirstScene(string name)
+    {
+        GameState.Instance.currentPlace = "desktop";
+        SceneManager.LoadScene(name);
+    }
+}

e7b661168a643e4d24caef627b2b6b3d2eefdc24
Agueda Gomez <gmz.cambronero@gmail.com>
2019-05-29 10:06:11 +0200

    Finish help

---
+++ Assets/Scripts/Scenes/MenuScene/HelpPanel/HelpChildManger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpChildManger : MonoBehaviour
+{
+    public void Hide()
+    {
+        transform.parent.GetComponent<HelpPanelManager>().Hide();
+        Destroy(gameObject);
+    }
+}

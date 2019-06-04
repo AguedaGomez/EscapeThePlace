13dd25bc0a457cec9420e93d3300fcbd13512fc2
Alberto Gonzalez Perez <algonzal@uji.es>
2019-06-04 16:39:20 +0200

    Add kick out behaviour when user leaves a place

--- Assets/Scripts/Scenes/StoryScene/StorySceneManager.cs
+++ Assets/Scripts/Scenes/StoryScene/StorySceneManager.cs
@@ -2,18 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using NearbyMessages;
 
 public class StorySceneManager : MonoBehaviour
 {
     public Canvas canvas;
     public PrefabsLoader prefabsLoader;
 
+    private NearbyMessagesEventSystem _nearbyMessages;
     private GameObject currentScreen;
     
     // Start is called before the first frame update
     void Start()
     {
+        _nearbyMessages = NearbyMessagesEventSystem.Instance;
+        _nearbyMessages.OnNearbyMessageLost += OnPlaceOutOfRange;
         LoadScreen();
+    }
+
+    private void OnDestroy()
+    {
+        _nearbyMessages.OnNearbyMessageLost -= OnPlaceOutOfRange;
     }
 
     private void LoadScreen()
@@ -23,6 +33,11 @@ public class StorySceneManager : MonoBehaviour
         Instantiate(prefab, canvas.transform);
     }
 
-
+    private void OnPlaceOutOfRange(NearbyMessage message)
+    {
+        var currenPlace = GameState.Instance.currentPlace;
+        if (message.Content != currenPlace) return;
+        SceneManager.LoadScene("MenuScene");
+    }
 
 }
